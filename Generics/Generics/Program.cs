aada687f8715ab64a9bca17d6f3e9355ce2edaf0
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-21 21:53:49 +0530

    Add UC3-FindMaximumString

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -12,7 +12,7 @@ namespace Generics
         static void Main(string[] args)
         {
             FindMaximum integer = new FindMaximum();
-            Console.WriteLine("1.Maximum Integer \n2.Maximum Float Number");
+            Console.WriteLine("1.Maximum Integer \n2.Maximum Float Number  \n3.Maximum String");
             Console.WriteLine("Enter the option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -27,6 +27,12 @@ namespace Generics
                     Console.WriteLine("Finding maximum of three floating numbers");
                     float maximumFloat = integer.MaxFloat(12.3, 45.6, 8.45);
                     Console.WriteLine("Maximum Floating Number is : " + maximumFloat);
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    Console.WriteLine("Finding maximum of three strings");
+                    string maxString = integer.MaxString("Apple", "Peach", "Banana");
+                    Console.WriteLine("Maximum string is : " + maxString);
                     Console.ReadLine();
                     break;
             }
