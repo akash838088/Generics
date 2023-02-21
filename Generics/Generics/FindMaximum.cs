f535962cdc3fb5dc11cb9dff413a80634f785391
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-21 21:39:32 +0530

    Add Maximum Float Number

--- Generics/Generics/FindMaximum.cs
+++ Generics/Generics/FindMaximum.cs
@@ -8,24 +8,38 @@ namespace Generics
 {
     public class FindMaximum
     {
-        public class MaxOfNumbers
+        public int MaxInteger(int numOne, int numTwo, int numThree)
         {
-            public int MaxInteger(int numOne, int numTwo, int numThree)
+            if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0)
             {
-                if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0)
-                {
-                    return numOne;
-                }
-                if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0)
-                {
-                    return numTwo;
-                }
-                if (numThree.CompareTo(numOne) > 0 && numThree.CompareTo(numTwo) > 0)
-                {
-                    return numThree;
-                }
-                throw new Exception("numOne,numTwo,numThree are same");
+                return numOne;
             }
+            if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0)
+            {
+                return numTwo;
+            }
+            if (numThree.CompareTo(numOne) > 0 && numThree.CompareTo(numTwo) > 0)
+            {
+                return numThree;
+            }
+            throw new Exception("numOne,numTwo,numThree are same");
+        }
+        public float MaxFloat(double valueOne, double valueTwo, double valueThree)
+        {
+
+            if (valueOne.CompareTo(valueTwo) > 0 && valueOne.CompareTo(valueThree) > 0)
+            {
+                return (float)valueOne;
+            }
+            if (valueTwo.CompareTo(valueOne) > 0 && valueTwo.CompareTo(valueThree) > 0)
+            {
+                return (float)valueTwo;
+            }
+            if (valueThree.CompareTo(valueOne) > 0 && valueThree.CompareTo(valueTwo) > 0)
+            {
+                return (float)valueThree;
+            }
+            throw new Exception("valueOne,valueTwo,valueThree are same");
         }
     }
 }
