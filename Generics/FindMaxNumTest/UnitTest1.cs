f2bfefc56c447fc8f16b1c78aa9f683a37c43577
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-22 21:57:03 +0530

    Add MaxInt UnitTest

--- Generics/FindMaxNumTest/UnitTest1.cs
+++ Generics/FindMaxNumTest/UnitTest1.cs
@@ -11,23 +11,8 @@ namespace FindMaxNumTest
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
         {
-            int result = intFindMax.GenericValue(91, 88, 81);
+            int result = intFindMax.GenericValue(11, 88, 91);
             Assert.AreEqual(91, result);
-            //Assert.Pass();
-
-        }
-        [Test]
-        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnsecondMax()
-        {
-            int result = intFindMax.GenericValue(99, 56, 35);
-            Assert.AreEqual(99, result);
-            //Assert.Pass();
-        }
-        [Test]
-        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnthirdMax()
-        {
-            int result = intFindMax.GenericValue(43, 88, 82);
-            Assert.AreEqual(88, result);
             //Assert.Pass();
 
         }
